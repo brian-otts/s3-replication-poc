2a7118abc7324c72553331733eb61dc9180b70fd
Brian Otts <ottsbrian@gmail.com>
2025-08-29 08:20:13 -0500

    Initial commit

---
+++ test/log-aggregation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LogAggregation from '../lib/log-aggregation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/log-aggregation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LogAggregation.LogAggregationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
